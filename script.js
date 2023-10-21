caaf5004aa26ad1732eb79df6427b1abcee72dcf
luki3122 <luki3122@gmail.com>
2023-10-22 01:09:00 +0200

    detailed page

--- script.js
+++ script.js
@@ -13,48 +13,8 @@ $(".more-options-button").addEventListener("click", () => {
 });
 
 let totalItemsCount = 0;
-let data = [
-    {
-      "title": "The Science of Quantum Mechanics",
-      "description": "Explore the fundamental principles of quantum mechanics and its impact on our understanding of the universe."
-    },
-    {
-      "title": "Artificial Intelligence and Machine Learning",
-      "description": "Discover how AI and machine learning are shaping the future of technology and industry."
-    },
-    {
-      "title": "The History of Ancient Rome",
-      "description": "A journey through the rise and fall of one of the greatest empires in history, Ancient Rome."
-    },
-    {
-      "title": "Climate Change and Environmental Impact",
-      "description": "Learn about the pressing issue of climate change and its consequences for the planet."
-    },
-    {
-      "title": "Exploring the Mysteries of Deep Space",
-      "description": "Journey into the cosmos and uncover the mysteries of distant galaxies and black holes."
-    },
-    {
-      "title": "The World of Cryptocurrency and Blockchain",
-      "description": "Dive into the world of digital currency and the revolutionary technology of blockchain."
-    },
-    {
-      "title": "The Art of Renaissance Painting",
-      "description": "A study of the Renaissance period and the masterpieces of artists like Leonardo da Vinci and Michelangelo."
-    },
-    {
-      "title": "Culinary Delights of Mediterranean Cuisine",
-      "description": "Savor the flavors of Mediterranean cuisine and discover its rich culinary traditions."
-    },
-    {
-      "title": "Mindfulness and Stress Reduction",
-      "description": "Explore the benefits of mindfulness practices for reducing stress and improving mental well-being."
-    },
-    {
-      "title": "The Impact of 3D Printing on Manufacturing",
-      "description": "Learn how 3D printing is revolutionizing the manufacturing industry and its applications."
-    }
-]
+let data = []
+let detailedData = {}
 
 function showArticle()
 {
@@ -72,9 +32,9 @@ function hideArticle()
   $(".page-header").style.width = "100%";
 }
 
-function generateArticle(title, description, more)
+function generateArticle(i, title, description, more)
 { 
-    return `<div onclick="onArticleClick(event)" class="search-result-element search-result-element-transition">
+    return `<div onclick="onArticleClick(event, ${i})" class="search-result-element search-result-element-transition">
         <div class="title">${title}</div>
         <div class="description">${description}</div>
         <div class="more">${more}<div>
@@ -96,14 +56,17 @@ function releasePageBusy()
   $("#search-bar-icon").childNodes[3].hidden = true;
 }
 
-function onArticleClick(e)
+function onArticleClick(e, i)
 {
   showArticle();
+
   if (e.currentTarget.childNodes[5].classList.contains("opened")) {
     e.currentTarget.childNodes[5].classList.remove("opened")
   } else {
     e.currentTarget.childNodes[5].classList.add("opened")
   }
+
+  fetchDetails(i).then(() => $(".article-lookup").innerHTML = detailedData.data.textContent);
 }
 
 async function fetchQuery(query, sygnatura, sad, rodzajOrzeczenia, symbolSprawy)
@@ -171,7 +134,7 @@ async function displayResults()
       for ( let i = 0; i < data.length; i++ ) {
         await setTimeout(() => {
               let p = document.createElement("div");
-              p.innerHTML = generateArticle(data[i].courtCases.map(e=>e.caseNumber).join("; "), data[i].textContent, "")
+              p.innerHTML = generateArticle(i, data[i].courtCases.map(e=>e.caseNumber).join("; "), data[i].textContent, "")
               $(".search-result-list").appendChild(p.firstChild);
           }
           , 200)
@@ -193,6 +156,22 @@ async function displayResults()
     }
     
 }
+
+
+async function fetchDetails(i)
+{
+  const link = data[i].href;
+  if(!Object.hasOwn(data[i], "detailedData"))
+  {
+    const receivedData = await (fetch(link).then( e => e.json() ));
+    data[i].detailedData = receivedData;
+    console.log(receivedData)
+  }
+  
+  detailedData = data[i].detailedData;
+  
+}
+
 
 let lastScroll = window.scrollY;
 addEventListener("scroll", (event) => {
