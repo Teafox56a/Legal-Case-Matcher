5fe018c30176e4d737d1512d63964fb2303f4829
foealke <vezfx@wp.pl>
2023-10-21 23:19:22 +0200

    stuff

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ $(".more-options-button").addEventListener("click", () => {
     moreOptionsOpen=!moreOptionsOpen;
 });
 
-const data = [
+let totalItemsCount = 0;
+let data = [
     {
       "title": "The Science of Quantum Mechanics",
       "description": "Explore the fundamental principles of quantum mechanics and its impact on our understanding of the universe."
@@ -64,6 +65,21 @@ function generateArticle(title, description, more)
     </div>`
 }
 
+function lockPageBusy()
+{
+  $("#search-bar").disabled = true;
+  $("#search-bar-icon").childNodes[1].hidden = true;
+  $("#search-bar-icon").childNodes[3].hidden = false;
+  $("#pagination-nav").hidden = true;
+}
+
+function releasePageBusy()
+{
+  $("#search-bar").disabled = false;
+  $("#search-bar-icon").childNodes[1].hidden = false;
+  $("#search-bar-icon").childNodes[3].hidden = true;
+}
+
 function onArticleClick(e)
 {
   if (e.currentTarget.childNodes[5].classList.contains("opened")) {
@@ -73,31 +89,92 @@ function onArticleClick(e)
   }
 }
 
-let searchResults = false;
+async function fetchQuery(query, sygnatura, sad, rodzajOrzeczenia, symbolSprawy)
+{
+  const receivedData = await (fetch(
+    `https://www.saos.org.pl/api/search/judgments?pageSize=10&pageNumber=0&all=${query}&sortingField=JUDGMENT_DATE&sortingDirection=DESC`
+  ).then( e => e.json() ))
+  totalItemsCount = receivedData.info.totalResults;
+  data = [];
+  console.log(receivedData)
+  for ( const block of receivedData.items ) {
+    let ob = {}
+    ob["title"] = block.courtCases.map(e=>e.caseNumber).join("; ")
+    ob["description"] = block.textContent
+    data.push(ob)
+  }
+}
 
+let searchResults = false;
 function search()
 {
+  lockPageBusy();
   const query = $("#search-bar").value;
   const sygnatura = $("#sygnatura").value;
   const sad = $("#sad").value;
   const rodzajOrzeczenia = $("#rodzaj-orzeczenia").value;
   const symbolSprawy = $("#symbol-sprawy").value;
+  fetchQuery(query, sygnatura, sad, rodzajOrzeczenia, symbolSprawy).then( displayResults );
+  // console.log(query, sygnatura, sad, rodzajOrzeczenia, symbolSprawy)
+  
+}
 
-  console.log(query, sygnatura, sad, rodzajOrzeczenia, symbolSprawy)
-  displayResults();
+let selectedPage = 1;
+
+function onlyUnique(value, index, array) {
+  return array.indexOf(value) === index;
+}
+
+function generatePaddingButtons(selectedPage, maxSize, range)
+{
+  // const allPages = [...(new Array(maxSize))].map((_,i)=>i+1);
+  let idx = selectedPage-1;
+  let r = Math.floor(range/2);
+  let l = range - r;
+  let idxRight = Math.min(idx + r, maxSize);
+  let idxLeft = Math.max(idx - l, 1);
+  let res = []
+  for ( let i = idxLeft; i<=idxRight; i++ ) {
+    res.push(i);
+  }
+  res.push(1);
+  res.push(maxSize);
+  res = res.sort((a,b)=>a-b);
+  res = res.filter(onlyUnique)
+  return res;
 }
 
 async function displayResults() 
 {
+    releasePageBusy()
     searchResults = true;
     $(".page-header").style.marginTop = "2%";
     $(".search-result-list").innerHTML = ""
-    for ( let i = 0; i<data.length; i++ ) {
+    if ( data.length == 0 ) {
+      $(".search-result-list").innerHTML = `<div class="no-results"> Nic nie znaleziono... </div>`
+    } else {
+      for ( let i = 0; i<data.length; i++ ) {
         await setTimeout(() => {
-            let p = document.createElement("div");
-            p.innerHTML = generateArticle(data[i].title, data[i].description, data[i].description)
-            $(".search-result-list").appendChild(p);
+              let p = document.createElement("div");
+              p.innerHTML = generateArticle(data[i].title, data[i].description, data[i].description)
+              $(".search-result-list").appendChild(p);
+          }
+          , 200)
+      }
+      setTimeout( () => { 
+        $("#pagination-nav .pagination").innerHTML = "";
+        $("#pagination-nav .pagination").innerHTML += 
+        `<li class="page-item"><a class="page-link" href="#">1</a></li>`
+        for ( let i = 1; i<8; i++ ) {
+          $("#pagination-nav .pagination").innerHTML += 
+          `<li class="page-item"><a class="page-link" href="#">${selectedPage+i}</a></li>`
         }
-        , 200)
-    } 
+        $("#pagination-nav .pagination").innerHTML += 
+        `<li class="page-item"><a class="page-link" href="#">${Math.floor(totalItemsCount/10)+1}</a></li>`
+
+
+        $("#pagination-nav").hidden = false;
+      }, 1000 );
+    }
+    
 }
